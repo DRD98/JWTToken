e4312cf59e6f8d61837d1b6d5e5f43119154bc04
Divya Rose Davis <35791840+DRD98@users.noreply.github.com>
2022-10-13 12:23:11 +0530

    Delete Authorization directory

--- Authorization/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-# from clientauth import views
-# from rest_framework_simplejwt import views as jwt_views
-
-urlpatterns = [ path('user/', include('clientauth.urls'), name = 'redirection'), ]
